9daec321994e9a2cb99bfb050a983fccabec7cd5
nlavia <62187068+nlavia@users.noreply.github.com>
2020-03-19 18:43:32 -0300

    Se cambia a proyecto Maven, se agregan dependencias.

---
+++ src/test/SearchTest.java
@@ -0,0 +1,41 @@
+package test;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import com.project.pom.HomePage;
+
+class SearchTest {
+
+	
+	private WebDriver driver;
+	HomePage  homePage;
+		
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		homePage = new HomePage(driver);
+		driver = homePage.chromeWebDriverConnection();
+		homePage.visit("https://www.experta.com.ar/");
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+	
+	driver.close();
+	}
+
+	@Test
+	public void test() throws  InterruptedException{
+	homePage.searchText();
+	homePage.resultGridOn();
+	
+	Assert.assertEquals( "ART", homePage.resultText());
+	
+  			
+
+	}
+
+}
